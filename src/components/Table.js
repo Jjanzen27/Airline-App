6ae5d8ba6feace35ba78939c1ce76779cf9e23cf
Jenae Janzen <jenaejanzen@gmail.com>
2021-08-27 13:34:41 +0000

    add next page prev page functionality

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const Table = ({ rows, columns, format, perPage=25 }) => {
-  const [page, setPage] = useState(12);
+  const [page, setPage] = useState(0);
   
     
   const headerValues = columns.map(c => <th key={c.name}>{c.name}</th>);
@@ -16,6 +16,16 @@ const Table = ({ rows, columns, format, perPage=25 }) => {
   const currentRowsOnPage = () => {
     return rows.slice(start, end);
   };
+  
+  const previousPage = (event) => {
+    event.preventDefault();
+    setPage(page - 1);
+  }
+
+  const nextPage = (event) => {
+    event.preventDefault();
+    setPage(page + 1);
+  }
 
   return (
     <div>
@@ -27,7 +37,9 @@ const Table = ({ rows, columns, format, perPage=25 }) => {
           {currentRowsOnPage()}
         </tbody>
       </table>
-      <p>Showing {start+1}-{end+1} routes of {rows.length}</p>
+      <p>Showing {start+1}-{end} routes of {rows.length}</p>
+      <button disabled={page === 0} onClick={previousPage}>Previous Page</button>
+      <button disabled={end >= rows.length}onClick={nextPage}>Next Page</button>
     </div>
   );
 };
